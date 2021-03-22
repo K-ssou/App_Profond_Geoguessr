13100da8db292df645f581be7b438c23c2d5e0a5
K-ssou <cassandragilet.cg@gmail.com>
2021-03-22 19:58:06 +0100

    BDD finale

--- rename.py
+++ rename.py
@@ -5,13 +5,13 @@ import sys
 from datetime import datetime
 
 cpt = 1
-for image_file_name in os.listdir('C:\\Users\\Cassandra\\Desktop\\cours\\2A\\App_Profond\\Projet\\App_Profond_Geoguessr\\images\\Paris'):
+for image_file_name in os.listdir('C:\\Users\\Cassandra\\Desktop\\cours\\2A\\App_Profond\\Projet\\App_Profond_Geoguessr\\images\\test\\Tokyo\\'):
 
-    im = Image.open('C:\\Users\\Cassandra\\Desktop\\cours\\2A\\App_Profond\\Projet\\App_Profond_Geoguessr\\images\\Paris\\'+image_file_name)
+    im = Image.open('C:\\Users\\Cassandra\\Desktop\\cours\\2A\\App_Profond\\Projet\\App_Profond_Geoguessr\\images\\test\\Tokyo\\'+image_file_name)
     new_width  = 150
     new_height = 150
     im = im.resize((new_width, new_height), Image.ANTIALIAS)
-    im.save('C:\\Users\\Cassandra\\Desktop\\cours\\2A\\App_Profond\\Projet\\App_Profond_Geoguessr\\Images_resize\\Paris\\' + 'Paris_' +str(cpt) + '.tif')
+    im.save('C:\\Users\\Cassandra\\Desktop\\cours\\2A\\App_Profond\\Projet\\App_Profond_Geoguessr\\Images_resize\\test\\Tokyo\\' + 'Tokyo_' +str(cpt) + '.tif')
     cpt +=1
 
 print("All done")
